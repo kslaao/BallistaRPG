3507cfa2062c9853677d840ac266ea62bce45f66
kslaao <Yusong.Feng@mail.bcu.ac.uk>
2021-02-22 17:29:00 +0000

    health bar + item select

---
+++ Ballista Project/Assets/UI/PlayerClick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClick : MonoBehaviour
+{
+    public Camera cam;
+    public Interactable focus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                RemoveFocus();
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        focus = newFocus;
+    }
+
+    void RemoveFocus()
+    {
+        focus = null;
+    }
+}
---
+++ Ballista Project/Assets/UI/health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    public HealthBar healthbar;
+    int maxhealth;
+    int currenthealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxhealth = 20;
+        currenthealth = maxhealth;
+        healthbar.SetMaxHealth(maxhealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            currenthealth -= 1;
+            healthbar.SetHealth(currenthealth);
+        }
+    }
+}
