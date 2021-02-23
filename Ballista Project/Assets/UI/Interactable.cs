8e759937a62a51a8ac34cce7285945a918c134a2
kslaao <Yusong.Feng@mail.bcu.ac.uk>
2021-02-23 22:59:47 +0000

    change way of interact item

---
+++ Ballista Project/Assets/UI/Interactable.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform Player;
+    public Transform interactionTransform;
+
+
+    bool isFocus = false;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public virtual void Interact()
+    {
+
+    }
+
+    public void OnFocused()
+    {
+
+    }
+
+    public void OnDefocused()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(Player.position, interactionTransform.position) < 5)
+        {
+            isFocus = true;
+
+        }
+        else
+        {
+            isFocus = false;
+        }
+
+        if (isFocus == true)
+        {
+            Interact();
+        }
+    }
+}
---
+++ Ballista Project/Assets/UI/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+    void PickUp()
+    {
+        Destroy(gameObject);
+    }
+}
